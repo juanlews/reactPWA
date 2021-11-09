aa9a9e733964f225341bee542ff13a316dcb7425
juan luiz <juanlews@hotmail.com>
2021-11-08 23:16:42 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'antd';
+import { BrowserRouter as Router, Route, Redirect, Switch, Link } from 'react-router-dom';
+
+const routes = [
+  { path: '/home', component: TelaInicial },
+  { path: '/diario', component: DiarioDeAlimentacao },
+  { path: '/pesquisa', component: Pesquisa },
+  { path: '/adicionar', component: Adicionar },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <Router>
+            <div>
+              <Route exact path="/" component={() => (
+                <Redirect to={{ pathname: '/home' }} />
+              )} />
+              <Switch>
+                {routes.map((route, index) => (
+                  <Route path={route.path} component={route.component} key={index} />
+                ))}
+                <Route component={() => ('Página não encontrada!')} />
+              </Switch>
+            </div>
+        </Router>
+
   );
 }
 
+function TelaInicial (){
+  return (
+    <>
+      <div id='home' style={{'textAlign':'center'}}>
+        {
+          //<img src="pngwing.com.png" alt="logo" style="width:20vw; margin-top:5vh" />
+        }
+        <Link to={'diario'} >
+          <Button style={{ 'width': '80vw', 'height':'8vh', 'marginTop':'10vh', 'border': 'hidden', 'fontSize':'26px', 'background':'#4250b3', color: 'aliceblue', 'borderRadius' : '25px'}}>
+            Diario de alimentação
+          </Button>
+        </Link>
+
+        <Link to={'pesquisa'} >
+          <Button onClick={()=>{}} style={{'width':'80vw', height:'8vh', marginTop:'5vh',border:' hidden',fontSize:'26px',background:'#4250b3',color:'aliceblue',borderRadius:' 25px'}}>
+            Pesquisar
+          </Button>
+        </Link>
+
+        <Link to={'adicionar'} >
+          <Button onclick={()=>{}} style={{'width':'80vw', height:'8vh', marginTop:'5vh',border: 'hidden', fontSize:'26px', background:'#4250b3', color: 'aliceblue', borderRadius: '25px'}}>
+            Adicionar
+          </Button>
+        </Link>
+      </div>
+    </>
+  );
+}
+
+function DiarioDeAlimentacao(){
+  return (
+    <>
+    DIARIO
+    </>
+  );
+}
+function Pesquisa(){
+  return (
+    <>
+    PESQUISA
+    </>
+  );
+}
+function Adicionar(){
+  return (
+    <>
+    ADICIONAR
+    </>
+  );
+}
 export default App;
